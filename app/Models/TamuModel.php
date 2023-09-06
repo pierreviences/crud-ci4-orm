e6d50503609edee4332b2908cf3015837f145685
pierreviences <imaniyazid@gmail.com>
2023-09-06 09:11:06 +0700

    feat: Add Model

---
+++ app/Models/TamuModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TamuModel extends Model
+{
+    protected $table = 'tb_tamu';
+    protected $primaryKey = 'id';
+    protected $returnType = 'object';
+    protected $allowedFields = ['tgl_berkunjung', 'nama_tamu', 'alamat', 'no_telepon', 'keperluan'];
+
+}
+
+?>
